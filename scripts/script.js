9bc3a9f546a1fd3c86df723d860842b9fe907ac8
Bogdane Braoun <112286@ecole-it.com>
2026-02-25 22:21:35 +0100

    added json database + added creation of cards on the main page using the database info

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,34 @@
 // 
+const main_container = document.querySelector("#main_page_container"); // the container of th edivs on the main page
+const dataURL = "/data/database.json"; // json database local URL
+
+// On page loaded this function takes all the info from the json database and loads the main page
+addEventListener("DOMContentLoaded", async() =>{
+    try{
+
+        const response = await fetch(dataURL); //Standart fetch of the database
+        const data = await response.json(dataURL); // standart "json-ification" if the fetched data
+
+        return mainPageCreateDivs(data); // Here it is executing the function to load the divs on the main page
+        
+
+    }catch(err){ // Standart error catching and loging
+        console.log("Error with json database :" + err);
+    };
+});
+
+
+//Function to create the main page divs
+function mainPageCreateDivs(DATABASE){
+    DATABASE.start_page.forEach(element => { //For each element in the start_page data of the database
+
+    main_container.innerHTML += `
+        <div class = "main_page_card">
+            <h2>${element.title}</h2>
+            <img src="" alt="">
+            <p>${element.description}</p>
+        </div>` ; // Creating the div and adding it into the HTML file
+
+    });
+
+};
