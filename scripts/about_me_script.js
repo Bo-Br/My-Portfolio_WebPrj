ba4eab5c1bf68cf2f321e9a92b17fadbb85931ff
Bogdane Braoun <112286@ecole-it.com>
2026-02-26 12:04:55 +0100

    add 'about me' page basic render + reform json database - render + database

--- scripts/about_me_script.js
+++ scripts/about_me_script.js
@@ -1,5 +1,12 @@
 // 
-const main_container = document.querySelector("#main_page_container"); // the container of th edivs on the main page
+const zoneAbout = document.querySelector("#hero")
+const zoneHobbyes = document.querySelector("#hobbyes")
+const zoneQuote = document.querySelector("#quote")
+const zoneFunFacts = document.querySelector("#fun_facts")
+const zoneCountries = document.querySelector("#visited_coutries ")
+
+
+
 const dataURL = "/data/database.json"; // json database local URL
 
 // On page loaded this function takes all the info from the json database and loads the main page
@@ -7,9 +14,9 @@ addEventListener("DOMContentLoaded", async() =>{
     try{
 
         const response = await fetch(dataURL); //Standart fetch of the database
-        const data = await response.json(dataURL); // standart "json-ification" if the fetched data
+        const data = await response.json(dataURL); // standart "json-ification" of the fetched data
 
-        return mainPageCreateDivs(data); // Here it is executing the function to load the divs on the main page
+        return AboutMeCreatePage(data); // Here it is executing the function to entirely load the page
         
 
     }catch(err){ // Standart error catching and loging
@@ -20,15 +27,60 @@ addEventListener("DOMContentLoaded", async() =>{
 
 //Function to create the About me page
 function AboutMeCreatePage(DATABASE){
-    DATABASE.about_me.forEach(element => { //For each element in the start_page data of the database
+    console.log(DATABASE.about_me[1].hobbyes)
 
-    main_container.innerHTML += `
-        <div class = "main_page_card">
-            <h2>${element.title}</h2>
-            <img src="" alt="">
-            <p>${element.description}</p>
-        </div>` ; // Creating the div and adding it into the HTML file
+    const hero = DATABASE.about_me[0].hero 
+    const hobbyes = DATABASE.about_me[1].hobbyes
+    const quote = DATABASE.about_me[2].quote
+    const fun_facts = DATABASE.about_me[3].fun_facts
+    const countries = DATABASE.about_me[4].visited_countries
+// Adding the Hero Zone
+    zoneAbout.innerHTML = `
+        <img src="${hero.photo}" alt="">
+        <div>
+            <h3> Who am I? </h3>
+            <p> ${hero.fast_description} </p>
+        </div>
+        ` ;
+
+// adding the hobbyes
+    hobbyes.forEach(element => {
+        zoneHobbyes.innerHTML += `
+        <div class = "small-card">
+            <h3> ${element} </h3>
+        </div>
+        ` ;
+    });
+
+// Adding the quote zone
+    zoneQuote.innerHTML += `
+        <div>
+            <h3> Favorite quote : ${quote} </h3>
+        </div>
+        ` ;
+
+// Adding Fun Facts
+    fun_facts.forEach(fact => {
+        zoneFunFacts.innerHTML += `
+        <div class = "fact-card">
+            <h3> ${fact.title} </h3>
+            <img src="${fact.image}" alt="">
+            <p> ${fact.info_1} </p>
+            <p> ${fact.info_2} </p>
+        </div>
+        ` ;
 
     });
 
-};
+// Adding visited Countries
+
+    countries.forEach(country => {
+        zoneCountries.innerHTML += `
+        <div class = "small-card">
+            <h3> ${country} </h3>
+        </div>
+        ` ;
+
+    });
+
+    };
