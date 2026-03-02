9b2d42a7dcdf5526a8dba0d581e3d49eca290c86
Bogdane Braoun <112286@ecole-it.com>
2026-03-02 11:05:12 +0100

    begin work on skill page - structure

--- scripts/about_skills_script.js
+++ scripts/about_skills_script.js
@@ -1,5 +1,23 @@
-// 
-const main_container = document.querySelector("#main_page_container"); // the container of th edivs on the main page
+// #################################################################################################
+// ----------------------------- GATHER ALL THE SECTIONS IN VARIABLES ------------------------------
+// #################################################################################################
+
+
+
+const zoneHero = document.querySelector("#hero")
+const zoneDegrees = document.querySelector("#degrees")
+const zoneCapabilities = document.querySelector("#capabilities")
+const zoneWeb = document.querySelector("#webTech")
+const zoneLinks = document.querySelector("#links ")
+
+console.log(zoneDegrees)
+
+
+
+// #################################################################################################
+// ------------------------------------ DATABASE DATA GATHERING ------------------------------------
+// #################################################################################################
+
 const dataURL = "/data/database.json"; // json database local URL
 
 // On page loaded this function takes all the info from the json database and loads the main page
@@ -7,10 +25,11 @@ addEventListener("DOMContentLoaded", async() =>{
     try{
 
         const response = await fetch(dataURL); //Standart fetch of the database
-        const data = await response.json(dataURL); // standart "json-ification" if the fetched data
+        const data = await response.json(dataURL); // standart "json-ification" of the fetched data
 
-        return mainPageCreateDivs(data); // Here it is executing the function to load the divs on the main page
-        
+
+        return AboutMeCreatePage(data); // Here it is executing the function to entirely load the page
+
 
     }catch(err){ // Standart error catching and loging
         console.log("Error with json database :" + err);
@@ -18,17 +37,54 @@ addEventListener("DOMContentLoaded", async() =>{
 });
 
 
-//Function to create the main page divs
-function mainPageCreateDivs(DATABASE){
-    DATABASE.start_page.forEach(element => { //For each element in the start_page data of the database
+//Function to create the About me page
+function AboutMeCreatePage(DATABASE){
+// Take all the info and put it inside variables
 
-    main_container.innerHTML += `
-        <div class = "main_page_card">
-            <h2>${element.title}</h2>
-            <img src="" alt="">
-            <p>${element.description}</p>
-        </div>` ; // Creating the div and adding it into the HTML file
-
-    });
+    const degrees = DATABASE.about_tech.stack.degrees;
+    const capabilities = DATABASE.about_tech.capabilities;
+    const webCards = DATABASE.about_tech.stack.backend
+    console.log(webCards);
+    createDegrees(degrees);
+    createCapabilities(capabilities);
+    createWebCards(webCards);
+// Use all this info to create the page
+    
 
 };
+
+// #################################################################################################
+// ---------------------------------- SECTION CREATING FUNCTIONS -----------------------------------
+// #################################################################################################
+
+function createDegrees(degrees){
+    degrees.forEach(element => {
+        zoneDegrees.innerHTML += `
+        <div>
+            <p>- ${element}</p>
+        </div>
+        `
+    });
+}
+
+
+function createCapabilities(capabilities){
+    capabilities.forEach(element => {
+        zoneCapabilities.innerHTML += `
+        <div>
+            <p>- ${element}</p>
+        </div>
+        `
+    });
+}
+
+function createWebCards(webCards){
+    webCards.forEach(element => {
+        zoneWeb.innerHTML += `
+        <div class = "fact-card">
+            <h3>${element.name}</h3>
+            <p>${element.notes}</p>
+        </div>
+        `
+    });
+}
